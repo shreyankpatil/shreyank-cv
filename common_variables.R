85401f55fdbfaa14cd4dd02531f9042e90c806da
shreyankpatil <shreyankpatil@gmail.com>
2020-06-18 00:10:32 +1200

    first commit

---
+++ common_variables.R
@@ -0,0 +1,7 @@
+# Common non-csv data between CV and resume
+
+# Header with description of what is trying to be accomplished with this CV/Resume
+intro_text <- "Data scientist who is adept at collecting, analysing and interpreting datasets and developing machine learning models. Strong analytical skills, attention to detail and ability to work successfully in a team environment.
+
+Currently seeking opportunities in progressive, technology focused organisations. "
+
