a96e6477149dc72c44633427b45ad20763e76ee7
Shreyank <shreyankpatil@gmail.com>
2021-02-17 23:35:03 +1300

    minor change to description to suit alternate version

--- common_variables.R
+++ common_variables.R
@@ -1,7 +1,7 @@
 # Common non-csv data between CV and resume
 
 # Header with description of what is trying to be accomplished with this CV/Resume
-intro_text <- "I am a Data Scientist who is passionate about collecting, analysing and telling stories with data. I pride myself on my ability to combine technical fundamentals with domain knowledge to deliver exemplary results for customers.
+intro_text <- "I am a Data Scientist who is passionate about collecting, analysing and telling stories with data. I pride myself on my ability to combine technical fundamentals with domain knowledge to deliver exemplary results for clients.
 
 I'm interested in roles in progressive, technology focused organisations."
 
