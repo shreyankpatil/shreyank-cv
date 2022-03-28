2e54d7e8f7f9c5bbbeec0da72a168fad1e38e436
Shreyank <shreyankpatil@gmail.com>
2022-03-28 22:20:39 +1100

    cv update 28.03

--- common_variables.R
+++ common_variables.R
@@ -1,8 +1,10 @@
 # Common non-csv data between CV and resume
 
 # Header with description of what is trying to be accomplished with this CV/Resume
-intro_text <- "I am a Data Scientist who is passionate about collecting, analysing and telling stories with data. I pride myself on my ability to combine technical fundamentals with domain knowledge to deliver exemplary results for customers.
+intro_text <- "I am a Data Scientist who is passionate about solving business problems using technology. I particularly enjoy building and automating machine learning solutions for both production and experimentation.
 
-I'm interested in roles in progressive, technology focused organisations."
+Throughout my career I have used the AWS stack extensively, including tools such as Sagemaker ML, ECS, EC2 and Athena. I have also had experience containerizing code to run machine learning experiments, and building pipelines to serve web applications.
+
+I am currently looking for Data Scientist roles where I can work with an interesting tech stack to build awesome solutions. "
 
 
